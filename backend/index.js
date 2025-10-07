29d6a23ab68cb6828e4dc3ca44810a0c6a26e2e4
PabloPianelo <pablopianeloxd@gmail.com>
2025-10-07 12:30:34 +0200

    ejemplo bakent

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
-const express = require("express");
+import express from "express";
+import usersRouter from "./example/routers/users.routing.js";
+// import  prisma  from "./.lib/prisma.js";
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use(express.json());
+app.use("/api/users", usersRouter);
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
 
 app.listen(4000, () => {
-    console.log("Server started on port 4000");
+  console.log("Server started on port 4000");
 });
