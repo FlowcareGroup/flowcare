21ef416807ed69b947811e1c90181653c068e386
Jeremie <jeremiechiclote34@gmail.com>
2025-10-09 16:26:17 +0200

    Cambios en controller pacientes

--- backend/src/controllers/patients.controller.js
+++ backend/src/controllers/patients.controller.js
@@ -1,6 +1,13 @@
 import { PrismaClient } from '../../generated/prisma/index.js';
 const prisma = new PrismaClient();
- 
 
-const PatientsController = {};
+ const getAllPatients = async (req,res)=>{
+    console.log("getAllPatients");
+ }
+
+ const createPatient = async (req,res)=>{
+    console.log("createPatient");
+ }
+
+const PatientsController = {getAllPatients,createPatient};
 export default PatientsController;
