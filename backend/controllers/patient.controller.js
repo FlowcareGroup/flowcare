e41dd2d9834aa4c46346fcde686bb6d845bee38b
PabloPianelo <pablopianeloxd@gmail.com>
2025-10-09 13:10:08 +0200

    estrutura Bakct

---
+++ backend/controllers/patient.controller.js
@@ -0,0 +1,12 @@
+import { PrismaClient } from '../../generated/prisma/index.js';
+const prisma = new PrismaClient();
+
+
+
+
+
+
+const PatientController = { };
+
+
+export default PatientController;
