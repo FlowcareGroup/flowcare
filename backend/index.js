d2f91d01f882df279f7e0593527b9da1e8a64199
Jeremie <jeremiechiclote34@gmail.com>
2025-10-09 15:53:11 +0200

    Cambios en dev

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import usersRouter from "./example/routers/users.routing.js";
-import PatientRouter from "./routers/Patient.Router.js";
+
 // import  prisma  from "./.lib/prisma.js";
 const app = express();
 
 app.use(express.json());
 app.use("/api/users", usersRouter);
-app.use("/patient", PatientRouter);
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
 // });
