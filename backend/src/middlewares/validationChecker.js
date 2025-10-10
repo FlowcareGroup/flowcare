6b7b77dc934726ae9c3a44d2e8de4e1d7c739355
PabloPianelo <pablopianeloxd@gmail.com>
2025-10-10 11:54:12 +0200

    Add validation for login endpoint and implement validationChecker middleware

---
+++ backend/src/middlewares/validationChecker.js
@@ -0,0 +1,14 @@
+import { validationResult } from "express-validator";
+
+// middleware que lee los errores y los devuelve en la respuesta, si los hubiera
+const validationChecker = (req, res, next) => {
+  const result = validationResult(req);
+  const errors = result.array();
+  if (errors.length > 0) {
+    return res.status(400).json(errors);
+  }
+
+  next();
+};
+
+export default validationChecker;
--- backend/src/routers/patients.routing.js
+++ backend/src/routers/patients.routing.js
@@ -1,5 +1,8 @@
 import express from "express";
-import PatientsController from "../controllers/patients.controller.js";
+import PatientsController from '../controllers/patients.controller.js';
+import { body } from 'express-validator';
+import validationChecker from '../middlewares/validationChecker.js';
+
 const router = express.Router();
 
 router.use(express.json());
@@ -7,7 +10,21 @@ router.use(express.json());
 router.get("/", PatientsController.getAllPatients);
 router.post("/", PatientsController.createPatient);
 //POST /api/patients/login
-router.post("/login", PatientsController.loginPatient);
+router.post("/login",[
+    body("email").isEmail().withMessage("Email no válido"),
+    // body("password")
+    //   .isLength({ min: 6 })
+    //   .withMessage("La contraseña debe tener al menos 6 caracteres")
+    //   .matches(/[0-9]/)
+    //   .withMessage("La contraseña debe contener al menos un número")
+    //   .matches(/[!@#$%^&*(),.?":{}|<>]/)
+    //   .withMessage("La contraseña debe contener al menos un carácter especial")
+    //   .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
+    //   .withMessage(
+    //     "La contraseña debe contener al menos una mayúscula, una minúscula"
+    //   ),
+    validationChecker,
+  ], PatientsController.loginPatient);
 
 
 
