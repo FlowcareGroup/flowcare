5126af2b6db63c5443e07d199ec93840ed5d7523
Carlos Damota <cdamota.cd@gmail.com>
2025-10-28 21:28:24 +0100

    fix: enhance UI consistency and readability in Auth components; adjust padding, font sizes, and error message styles for better user experience

--- frontend/src/app/(auth)/auth/layout.tsx
+++ frontend/src/app/(auth)/auth/layout.tsx
@@ -6,12 +6,18 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[url('/fc7257a5bffaea6a2bc3afa30989fadd7e413f85.jpg')] bg-cover bg-center bg-linear-300 ">
-      <div className="flex items-center bg-white border border-gray-300 shadow-lg rounded-lg  p-4 flex-col w-md ">
-        <Image src="/iconFlowCare.svg" alt="FlowCare Logo" width={250} height={250}  />
-        <p className="text-xl font-normal text-center py-0 px-12 text-gray-800">Te damos la bienvenida a tu 
-espacio de salud online</p>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[url('/fc7257a5bffaea6a2bc3afa30989fadd7e413f85.jpg')] bg-cover bg-center">
+      <div className="flex items-center bg-white border border-gray-300 shadow-lg rounded-lg p-4 md:p-8 flex-col w-full mx-4 max-w-md md:max-w-lg">
+        <Image 
+          src="/iconFlowCare.svg" 
+          alt="FlowCare Logo" 
+          width={250} 
+          height={250}
+          className="w-32 md:w-64 h-auto"
+        />
+        <p className="text-lg md:text-xl font-normal text-center py-4 px-4 md:px-12 text-gray-800">
+          Te damos la bienvenida a tu espacio de salud online
+        </p>
         {children}
       </div>
     </div>
